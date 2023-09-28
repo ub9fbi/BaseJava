ae24e63ca859983c32ca1c93d93e8a9ff634e3e3
ub9fbi <zolotoianatolji@yandex.com>
2023-09-28 16:02:56 +0500

    1

---
+++ src/ArrayStorage.java
@@ -0,0 +1,27 @@
+public class ArrayStorage {
+    Resume[] storage = new Resume[10000];
+
+    void clear() {
+    }
+
+    void save(Resume r) {
+    }
+
+    Resume get(String uuid) {
+        return null;
+    }
+
+    void delete(String uuid) {
+    }
+
+    /**
+     * @return array, contains only Resumes in storage (without null)
+     */
+    Resume[] getAll() {
+        return new Resume[0];
+    }
+
+    int size() {
+        return 0;
+    }
+}
